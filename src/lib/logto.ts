7bd9821115151c42c8f2a2d463caad0dec14551d
theCephas <oshoiseoluwa@gmail.com>
2024-10-02 17:38:33 +0100

    first commit

---
+++ src/lib/logto.ts
@@ -0,0 +1,8 @@
+export const logtoConfig = {
+  endpoint: "https://auth.fleetmanager.live/",
+  appId: "j7w1fn4rwizbys3q71kwe",
+  appSecret: "lWLQRyhAQwdg7WaGUcdF0w3Y01IeWn4E",
+  baseUrl: "http://localhost:3000", // Change to your own base URL
+  cookieSecret: "vCXPSoNYB5rF7jrOuWJ0ih8ZJC0OCHq0", // Auto-generated 32 digit secret
+  cookieSecure: process.env.NODE_ENV === "production",
+};
